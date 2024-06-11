4185e8f81034059d29d38471f09e8632339871c9
AHAdd40451 <ahadkhan40451@gmail.com>
2024-06-11 17:38:27 +0500

    first commit

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import nc, { NextHandler } from "next-connect";
+
+function someMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: NextHandler
+) {
+  next(); // call to proceed to next in chain
+}
+
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+})
+  .use(someMiddleware)
+  .get((req, res) => {
+    res.send("Hello world");
+  })
+  .post((req, res) => {
+    res.json({ hello: "world" });
+  })
+  .put(async (req, res) => {
+    res.end("async/await is also supported!");
+  })
+  .patch(async (req, res) => {
+    throw new Error("Throws me around! Error can be caught and handled.");
+  });
+
+export default handler;
